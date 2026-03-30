b8ce675e8c9ec3da840e1bc27f7bbac21797bc75
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2026-03-30 20:39:26 +0300

    first commit

---
+++ intro/src/main/java/com/turkcell/Main.java
@@ -0,0 +1,7 @@
+package com.turkcell;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hellö Wörld! Tamer'in Java Eğitimine Hoşgeldiniz!");
+    }
+}
