f8a3f9e59241d74f3e43b4ab57db3c227e091023
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2026-04-01 20:25:58 +0300

    Primitive vs Non-primitive (Referans) Tipler

--- intro/src/main/java/com/turkcell/Main.java
+++ intro/src/main/java/com/turkcell/Main.java
@@ -28,5 +28,29 @@ public class Main {
         boolean isStudent = true; 
         char grade = 'A'; 
 
+        // Diziler (Arrays)
+
+        String[] names = {"Tamer", "Ahmet", "Ayşe"};
+        System.out.println(names[0]); // Tamer
+
+        // Primitive (ilkel) tipler -> int, double, boolean, char
+        // Non-primitive (nesne) tipler -> Referans Tipler -> String, Arrays, Objects
+        int[] c = {1, 2, 3, 4};
+        int[] d = c;
+        d[3] = 100;
+        System.out.println(c[3]);
+        System.out.println(d[3]);
+
+        String str1 = "Hello";
+        String str2 = "Hello";
+        System.out.println(str1 == str2); // true (String havuzu (pool) nedeniyle)
+        // Aynı metinleri bir havuzda tutar ve aynı referansı paylaşır
+
+        System.out.println(str1.equals(str2)); // true (değer karşılaştırması)
+
+        String str3 = "Turkcell";
+        String str4 = str3.intern(); // Havuzda aynı değere sahip bir String varsa onu kullanır
+        System.out.println(str3 == str4); // true (intern() ile havuzdaki referansı kullanır)
+
     }
 } // Main classının kapsama alanı (sınır)
