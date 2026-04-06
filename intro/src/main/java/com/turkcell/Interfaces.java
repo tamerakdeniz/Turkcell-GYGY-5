aa56bfee15b4e28fe8708c4e3b0aeab98cee7db0
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2026-04-06 20:46:29 +0300

    task added.

--- intro/src/main/java/com/turkcell/Interfaces.java
+++ intro/src/main/java/com/turkcell/Interfaces.java
@@ -10,3 +10,15 @@ public interface Interfaces {
 
     }
 }
+/*
+Yeni bir proje oluşturmak (Aynı klasör içinde olabilir)
+isim: Bankingapplication
+
+Banka uygulaması => İçerik tamamen size ait.
+Min 3+ özellik. (Hesap özeti görme, veritabanı görme, müşteri olma vb.)
+In-Memory Repository => Veriler RAM'de tutulabilir
+Tek bir main classda simülasyon yeterli.
+Console üzerinden çalışacak.
+
+Çarşamba ders saatine kadar github teslimi.
+*/
