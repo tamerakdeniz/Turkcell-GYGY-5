9eb25cf5a1634e41474996e6144c4b4c4f071dd4
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2026-04-07 13:11:47 +0300

    feat: Customer class was added using Entity structure

---
+++ bankingapp/src/main/java/com/turkcell/Customer.java
@@ -0,0 +1,43 @@
+package com.turkcell;
+
+public class Customer {
+    private int id;
+    private String name;
+    private double balance;
+
+    // Default constructor
+    public Customer() {
+    }
+
+    // Constructor
+    public Customer(int id, String name, double balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    // Encapsulation
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
--- bankingapp/src/main/java/com/turkcell/Main.java
+++ bankingapp/src/main/java/com/turkcell/Main.java
@@ -2,6 +2,6 @@ package com.turkcell;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Uygulama girişi
     }
 }
