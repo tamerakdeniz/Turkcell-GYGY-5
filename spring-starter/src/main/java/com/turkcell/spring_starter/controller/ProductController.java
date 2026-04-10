9253025fd5d4ce38f1e26a805d48fe89938c618c
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2026-04-10 19:11:48 +0300

    feat: implement ProductController with CRUD operations

--- spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/controller/ProductController.java
@@ -1,14 +1,20 @@
 package com.turkcell.spring_starter.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.turkcell.spring_starter.model.Product;
 
+/*
 @RestController
 @RequestMapping("/api/product") // localhost:8080/api/products -> bu endpoint'e istek geldiğinde bu controller çalışır. ProductController
 // Bu class bir reset controller olduğunu belirtir. Yani bu class HTTP isteklerini karşılamak için kullanılır.
@@ -36,5 +42,45 @@ public class ProductController {
 
         return product;
     }
+    
 
+
+}
+*/
+
+@RestController 
+@RequestMapping("/api/product") 
+public class ProductController {
+    // In-Memory Çalış..
+    private List<Product> productList = new ArrayList<>();
+
+
+    @GetMapping()
+    public List<Product> getAllProducts() {
+        return productList;
+    }
+
+    @GetMapping("{id}")
+    public Product getProductById(@PathVariable int id) 
+    {
+        // Listeden id == product.getId() ise onu yoksa null dön.
+        return productList.stream().filter(i->i.getId() == id).findFirst().orElse(null);
+    }
+
+    @PostMapping
+    public void createProduct(@RequestBody Product product) {
+        productList.add(product);
+    }
+    @PutMapping
+    public void updateProduct(@RequestBody Product product) {
+        ///..
+        Product productToUpdate = productList.stream().filter(p -> p.getId() == product.getId()).findFirst().orElseThrow();
+
+        productToUpdate.setName(product.getName());
+        productToUpdate.setPrice(product.getPrice());
+    }
+    @DeleteMapping("{id}")
+    public void deleteProduct(@PathVariable int id) {
+        ///..
+    }
 }
