fcd11902eba43db81d3efda68bbb6eb832f588d6
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2026-05-08 13:52:11 +0300

    feat: Implement NotLoggableRequest interface in RegisterCommand to prevent logging of plain password hashes

--- library-cqrs/src/main/java/com/turkcell/library_cqrs/application/features/user/command/register/RegisterCommand.java
+++ library-cqrs/src/main/java/com/turkcell/library_cqrs/application/features/user/command/register/RegisterCommand.java
@@ -2,12 +2,14 @@ package com.turkcell.library_cqrs.application.features.user.command.register;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.turkcell.library_cqrs.core.logging.NotLoggableRequest;
 import com.turkcell.library_cqrs.core.mediator.cqrs.Command;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+// NotLoggableRequest -> plain password hash'lenmeden önce LoggingBehavior'a uğrar; konsola düşmesin.
 public record RegisterCommand(
     @NotBlank @Email String email,
     @NotBlank @Length(min=3) String password
-) implements Command<RegisterResponse> {}
+) implements Command<RegisterResponse>, NotLoggableRequest {}
