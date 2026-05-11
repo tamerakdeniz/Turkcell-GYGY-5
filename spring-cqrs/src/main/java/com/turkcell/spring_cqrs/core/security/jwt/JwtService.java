32d51184abf63c46f2c941bbd8409a60abe1ef58
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2026-05-11 19:06:33 +0300

    feat: Add methods to extract user ID and email from JWT and validate token

--- spring-cqrs/src/main/java/com/turkcell/spring_cqrs/core/security/jwt/JwtService.java
+++ spring-cqrs/src/main/java/com/turkcell/spring_cqrs/core/security/jwt/JwtService.java
@@ -3,12 +3,14 @@ package com.turkcell.spring_cqrs.core.security.jwt;
 import java.time.Instant;
 import java.util.Date;
 import java.util.UUID;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -39,4 +41,31 @@ public class JwtService {
                    .signWith(this.signingKey)
                    .compact();
     } 
+
+
+    public String extractUserId(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String extractEmail(String token) {
+        return extractClaim(token, claims -> claims.get("email", String.class));
+    }   
+
+    public boolean isTokenValid(String token) {
+        try {
+            return !extractClaim(token, Claims::getExpiration).before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> resolver) {
+        Claims claims = Jwts.parser()
+                            .verifyWith(this.signingKey)
+                            .build()
+                            .parseSignedClaims(token)
+                            .getPayload();
+        return resolver.apply(claims);
+    }
+
 }
